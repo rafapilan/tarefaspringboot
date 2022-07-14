073883f3e57b8b086b617597a9d786b0e143691b
Rafael Ludovig Pilan <rafael.pilan@spassu.com.br>
2022-07-13 23:53:23 -0300

    Create Project Backend

---
+++ backend/src/main/java/com/rafapilan/tarefaspringboot/TarefaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rafapilan.tarefaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaspringbootApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafapilan/tarefaspringboot/TarefaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafapilan.tarefaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
